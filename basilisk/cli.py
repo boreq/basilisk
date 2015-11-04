070a508d8198ac51ff24ba12eac968bf1dde18ef
boreq <boreq@sourcedrops.com>
2015-11-04 18:11:34 +0100

    Fixed a bug in cli

--- basilisk/cli.py
+++ basilisk/cli.py
@@ -12,16 +12,16 @@ logger = logging.getLogger('cli')
 @click.option('--debug/--no-debug', default=False)
 @click.pass_context
 def cli(ctx, debug):
-    ctx.obj['DEBUG'] = debug
+    ctx.obj = {'DEBUG': debug}
     log_format = (debug and DEBUG_LOG_FORMAT) or LOG_FORMAT
     log_level = (debug and logging.DEBUG) or logging.INFO
     logging.basicConfig(format=log_format, level=log_level)
 
 
 @cli.command()
-@click.pass_context
 @click.argument('source_directory', type=click.Path(exists=False, resolve_path=True))
 @click.argument('output_directory', type=click.Path(resolve_path=True))
+@click.pass_context
 def build(ctx, source_directory, output_directory):
     """Builds the project."""
     try:
