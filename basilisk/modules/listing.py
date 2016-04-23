6f514ce8907e7f31f6a732bd39cad71230189e21
boreq <boreq@sourcedrops.com>
2016-04-23 19:23:56 +0200

    Listing: fixed after the changes in the Builder and Build classes

--- basilisk/modules/listing.py
+++ basilisk/modules/listing.py
@@ -3,8 +3,44 @@ from . import Module
 
 
 class ListingModule(Module):
-    """Adds additional context with lists of files present in a certain
-    directory.
+    """Adds additional context with lists of files which will be created in the
+    output directory. Those lists can be used to create file listings such as
+    navigation menus or article lists in the templates. The following keys are
+    additionaly available in the context: 'listing', 'current_listing'.
+
+    Example of the additional context:
+
+        {
+            'listing': {
+                'about': {
+                    'type': 'directory',
+                    'content': {
+                        'index.html': {
+                            'type': 'file',
+                            'parameters': {'title': 'Contact'}
+                        }
+                    }
+                },
+                'index.html': {
+                    'type': 'file'},
+                    'parameters': {'title': 'Simple example'}
+                }
+            },
+            'current_listing': {
+                'index.html': {
+                    'type': 'file',
+                    'parameters': {'title': 'Contact'}
+                }
+            }
+        }
+
+    The listing shows all files which most likely will be present in the output
+    directory (it is possible that a module which runs after this one will
+    modify them). Since this example listing is shown for a file
+    'about/index.html', the current listing provides a shortcut to view the list
+    of files in that particular directory (only that file is present however).
+
+    This module doesn't require any additional configuration.
     """
 
     priority = -5
@@ -21,9 +57,18 @@ class ListingModule(Module):
                         'content': {}
                     }
                 current = current[part]['content']
+
+            # Here we have to cheat a little to get the params by reading the
+            # file at this point.
+            inpath = os.path.join(self.builder.source_directory, build.input_path)
+            lines = build.read(inpath)
+            content, parameters = build.parse_lines(lines)
+
             current[parts[-1]] = {
                 'type': 'file',
-                'parameters': build.parameters
+                'parameters': parameters
             }
+
+            # Put the created listings the the additional context.
             build.additional_context['listing'] = listing
             build.additional_context['current_listing'] = current
