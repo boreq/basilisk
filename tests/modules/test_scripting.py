7f7be246c492f8c96d9444819b7f302f3bb5aa81
boreq <boreq@0x46.net>
2019-02-08 00:38:56 +0100

    Add scripting module tests

---
+++ tests/modules/test_scripting.py
@@ -0,0 +1,39 @@
+import pytest
+import subprocess
+from basilisk.modules.scripting import ScriptingModule
+from basilisk.build import Build
+
+
+def test_no_config(builder):
+    module = ScriptingModule()
+    module.builder = builder
+    module.preprocess(None)
+
+
+def test_command(builder):
+    builder.config['module_config']['scripting'] = {
+        'scripts': ['echo testing']
+    }
+    module = ScriptingModule()
+    module.builder = builder
+    module.preprocess(None)
+
+
+def test_invalid_command(builder):
+    builder.config['module_config']['scripting'] = {
+        'scripts': ['invalid command']
+    }
+    module = ScriptingModule()
+    module.builder = builder
+    with pytest.raises(subprocess.CalledProcessError):
+        module.preprocess(None)
+
+
+def test_replacement(builder):
+    text_in = 'command ${source_directory} ${output_directory}'
+    text_out = 'command source_directory output_directory'
+
+    module = ScriptingModule()
+    module.builder = builder
+    out = module.replace_placeholders(text_in)
+    assert out == text_out
