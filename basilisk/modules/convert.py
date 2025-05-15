95855cf92e74cee9a73ee0345d84731b9f5ad03c
boreq <boreq@0x46.net>
2025-05-15 04:13:44 +0200

    I think changing paths in the processor breaks something?

--- basilisk/modules/convert.py
+++ basilisk/modules/convert.py
@@ -30,6 +30,9 @@ class ConvertModule(Module):
         raise Exception('convert module doesn\'t support converting to \'{}\''.format(to))
 
     def execute(self, build, module_config):
+        output_path = pathlib.Path(build.output_path)
+        build.output_path = os.path.join(output_path.parent, '{}.webp'.format(output_path.stem))
+
         processor = self.make_processor(build, module_config)
         build.processors.append(processor)
 
@@ -39,9 +42,6 @@ class WebpProcessorBuilder:
     @staticmethod
     def make_processor(module, build, module_config):
         def processor(content, *args, **kwargs):
-            output_path = pathlib.Path(build.output_path)
-            build.output_path = os.path.join(output_path.parent, '{}.webp'.format(output_path.stem))
-
             with tempfile.TemporaryDirectory() as directory:
                 path_in = os.path.join(directory, 'input_file')
                 path_out = os.path.join(directory, 'output_file.webp')
@@ -61,6 +61,9 @@ class WebpProcessorBuilder:
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.DEVNULL,
                 )
+
+                if result.returncode != 0:
+                    raise Exception('cwebp error')
 
                 with open(path_out, 'rb') as f:
                     content = f.read()
